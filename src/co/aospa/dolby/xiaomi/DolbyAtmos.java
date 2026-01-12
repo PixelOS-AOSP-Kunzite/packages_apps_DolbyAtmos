UnsatifsedError/lineage_hardware_xiaomi

Introduce Dolby Atmos
Moved from marble/sm8450-common

History:

commit 82fe03168c0402e4cb10d25859c3b398c0ef654a
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Thu Mar 21 21:35:36 2024 +0530

    marble: parts: Restore dolby profile on audio changes

    Something keeps resetting back at random times, from what I observed,
    after resuming media or on a device change, lets workaround that.

    Change-Id: Id065f2482636194655c2399f0c35ad56b8e7a29d

commit c4400bd1326f65aeac1d0f26bb830ce7fd079773
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Fri Feb 2 09:29:08 2024 +0530

    marble: parts/keyhandler: Guard debug logging

    Change-Id: I246941f26cd1f71b696eb3c996794c9baa5dbc00

commit f11b70a98a11d0b89673d73002996aed9f11fbd7
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Sun Dec 31 20:36:52 2023 +0530

    marble: parts: Re-enable speaker virtualization after bootup

    For whatever reason, speaker virtualization isn't automatically
    restored at bootup unlike the other parameters. It was reported to be
    fixed by connecting and disconnecting headphones or disabling and
    enabling the toggle, so let's just automate that at bootup.

commit abcff4fb947c89b69c1d25bd290fd91b7873af6a
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Fri Oct 20 06:49:19 2023 +0530

    marble: parts: Implement profile-specific Dolby settings

    Some refactoring and cleanup while we're at it.

commit dc54f9ddeff212d017b0cba16e56516e99335bb3
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Mon Oct 9 21:58:58 2023 +0530

    marble: parts: Remove play/pause hack while toggling Dolby

    Not required with/fixed by:
    35217: audioflinger: Do not allow DAP effect to be suspended | https://gerrit.aospa.co/c/AOSPA/android_frameworks_av/+/35217

commit dd2acc8e0c10d05f86ff229412cc9f72ea242b44
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Wed Sep 13 21:41:20 2023 +0530

    marble: parts: Set proper summar

src/com/afterlifeos/dolby/afterlife/DolbyAtmos.java
@@ -1,0 +1,156 @@
+/*
+ * Copyright (C) 2024 AfterlifeOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.afterlifeos.dolby.afterlife;
+
+import android.media.audiofx.AudioEffect;
+import android.util.Log;
+
+import com.afterlifeos.dolby.afterlife.DolbyConstants.DsParam;
+
+import java.util.UUID;
+
+class DolbyAtmos extends AudioEffect {
+
+    private static final String TAG = "DolbyAtmos";
+    private static final UUID EFFECT_TYPE_DAP =
+            UUID.fromString("9d4921da-8225-4f29-aefa-39537a04bcaa");
+
+    private static final int
+        EFFECT_PARAM_CPDP_VALUES = 5,
+        EFFECT_PARAM_ENABLE = 0,
+        EFFECT_PARAM_PROFILE = 0xA000000,
+        EFFECT_PARAM_SET_PROFILE_PARAMETER = 0x1000000,
+        EFFECT_PARAM_GET_PROFILE_PARAMETER = 0x1000005,
+        EFFECT_PARAM_RESET_PROFILE_SETTINGS = 0xC000000;
+
+    DolbyAtmos(int priority, int audioSession) {
+        super(EFFECT_TYPE_NULL, EFFECT_TYPE_DAP, priority, audioSession);
+    }
+
+    private static int int32ToByteArray(int value, byte[] dst, int index) {
+        dst[index++] = (byte) (value & 0xff);
+        dst[index++] = (byte) ((value >>> 8) & 0xff);
+        dst[index++] = (byte) ((value >>> 16) & 0xff);
+        dst[index] = (byte) ((value >>> 24) & 0xff);
+        return 4;
+    }
+
+    private static int byteArrayToInt32(byte[] ba) {
+        return ((ba[3] & 0xff) << 24) | ((ba[2] & 0xff) << 16)
+                | ((ba[1] & 0xff) << 8) | (ba[0] & 0xff);
+    }
+
+    private static int int32ArrayToByteArray(int[] src, byte[] dst, int index) {
+        for (int x : src) {
+            dst[index++] = (byte) ((x >>> 0) & 0xff);
+            dst[index++] = (byte) ((x >>> 8) & 0xff);
+            dst[index++] = (byte) ((x >>> 16) & 0xff);
+            dst[index++] = (byte) ((x >>> 24) & 0xff);
+        }
+        return src.length << 2;
+    }
+
+    private static int[] byteArrayToInt32Array(byte[] ba, int dstLength) {
+        int srcLength = ba.length >> 2;
+        if (dstLength > srcLength) {
+            dstLength = srcLength;
+        }
+        int[] dst = new int[dstLength];
+        for (int i = 0; i < dstLength; i++) {
+            dst[i] = ((ba[i * 4 + 3] & 0xff) << 24) | ((ba[i * 4 + 2] & 0xff) << 16)
+                    | ((ba[i * 4 + 1] & 0xff) << 8) | (ba[i * 4] & 0xff);
+        }
+        return dst;
+    }
+
+    private void setIntParam(int param, int value) {
+        byte[] buf = new byte[12];
+        int i = int32ToByteArray(param, buf, 0);
+        int32ToByteArray(value, buf, i + int32ToByteArray(1, buf, i));
+        checkStatus(setParameter(EFFECT_PARAM_CPDP_VALUES, buf));
+    }
+
+    private int getIntParam(int param) {
+        byte[] buf = new byte[12];
+        int32ToByteArray(param, buf, 0);
+        checkStatus(getParameter(EFFECT_PARAM_CPDP_VALUES + param, buf));
+        return byteArrayToInt32(buf);
+    }
+
+    void setDsOn(boolean on) {
+        setIntParam(EFFECT_PARAM_ENABLE, on ? 1 : 0);
+        super.setEnabled(on);
+    }
+
+    boolean getDsOn() {
+        return getIntParam(EFFECT_PARAM_ENABLE) == 1;
+    }
+
+    void setProfile(int index) {
+        setIntParam(EFFECT_PARAM_PROFILE, index);
+    }
+
+    int getProfile() {
+        return getIntParam(EFFECT_PARAM_PROFILE);
+    }
+
+    void resetProfileSpecificSettings() {
+        int profile = getProfile();
+        dlog("resetProfileSpecificSettings: profile=" + profile);
+        setIntParam(EFFECT_PARAM_RESET_PROFILE_SETTINGS, profile);
+    }
+
+    void setDapParameter(int profile, DsParam param, int values[]) {
+        dlog("setDapParameter: profile=" + profile + " param=" + param);
+        int length = values.length;
+        byte[] buf = new byte[(length + 4) * 4];
+        int i = int32ToByteArray(EFFECT_PARAM_SET_PROFILE_PARAMETER, buf, 0);
+        int i2 = i + int32ToByteArray(length + 1, buf, i);
+        int i3 = i2 + int32ToByteArray(profile, buf, i2);
+        int32ArrayToByteArray(values, buf, i3 + int32ToByteArray(param.id, buf, i3));
+        checkStatus(setParameter(EFFECT_PARAM_CPDP_VALUES, buf));
+    }
+
+    void setDapParameter(DsParam param, int values[]) {
+        setDapParameter(getProfile(), param, values);
+    }
+
+    void setDapParameterBool(DsParam param, boolean enable) {
+        setDapParameter(param, new int[]{enable ? 1 : 0});
+    }
+
+    void setDapParameterInt(DsParam param, int value) {
+        setDapParameter(param, new int[]{value});
+    }
+
+    int[] getDapParameter(int profile, DsParam param) {
+        dlog("getDapParameter: profile=" + profile + " param=" + param);
+        int length = param.length;
+        byte[] buf = new byte[(length + 2) * 4];
+        int i = (param.id << 16) + EFFECT_PARAM_GET_PROFILE_PARAMETER;
+        checkStatus(getParameter(i + (profile << 8), buf));
+        return byteArrayToInt32Array(buf, length);
+    }
+
+    int[] getDapParameter(DsParam param) {
+        return getDapParameter(getProfile(), param);
+    }
+
+    boolean getDapParameterBool(DsParam param) {
+        return getDapParameter(param)[0] == 1;
+    }
+
+    int getDapParameterInt(DsParam param) {
+        return getDapParameter(param)[0];
+    }
+
+    private static void dlog(String msg) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, msg);
+        }
+    }
+}

src/com/afterlifeos/dolby/afterlife/SummaryProvider.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *           (C) 2024 AfterlifeOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.afterlifeos.dolby.afterlife;
+
+import static com.android.settingslib.drawer.TileUtils.META_DATA_PREFERENCE_SUMMARY;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.afterlifeos.dolby.afterlife.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+/** Provide preference summary for injected items. */
+public class SummaryProvider extends ContentProvider {
+
+    private static final String KEY_DOLBY = "dolby";
+
+    @Override
+    public Bundle call(String method, String uri, Bundle extras) {
+        final Bundle bundle = new Bundle();
+        String summary;
+        switch (method) {
+            case KEY_DOLBY:
+                summary = getDolbySummary();
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown method: " + method);
+        }
+        bundle.putString(META_DATA_PREFERENCE_SUMMARY, summary);
+        return bundle;
+    }
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+            String sortOrder) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        throw new UnsupportedOperationException();
+    }
+
+    private String getDolbySummary() {
+        final DolbyUtils dolbyUtils = DolbyUtils.getInstance(getContext());
+        final boolean dsOn = dolbyUtils.getDsOn();
+        if (!dsOn) {
+            return getContext().getString(R.string.dolby_off);
+        }
+        final String profileName = dolbyUtils.getProfileName();
+        if (profileName == null) {
+            return getContext().getString(R.string.dolby_on);
+        } else {
+            return getContext().getString(R.string.dolby_on_with_profile, profileName);
+        }
+    }
+
+}

PixelOS-AOSP-Kunzite/packages_apps_DolbyAtmos

Change-Id: I79841c045fe7b92c438177916f756faab72ff0e9

src/co/aospa/dolby/xiaomi/DolbyAtmos.java
@@ -1,0 +1,156 @@
+/*
+ * Copyright (C) 2023-24 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby.xiaomi;
+
+import android.media.audiofx.AudioEffect;
+import android.util.Log;
+
+import co.aospa.dolby.xiaomi.DolbyConstants.DsParam;
+
+import java.util.UUID;
+
+class DolbyAtmos extends AudioEffect {
+
+    private static final String TAG = "DolbyAtmos";
+    private static final UUID EFFECT_TYPE_DAP =
+            UUID.fromString("9d4921da-8225-4f29-aefa-39537a04bcaa");
+
+    private static final int
+        EFFECT_PARAM_CPDP_VALUES = 5,
+        EFFECT_PARAM_ENABLE = 0,
+        EFFECT_PARAM_PROFILE = 0xA000000,
+        EFFECT_PARAM_SET_PROFILE_PARAMETER = 0x1000000,
+        EFFECT_PARAM_GET_PROFILE_PARAMETER = 0x1000005,
+        EFFECT_PARAM_RESET_PROFILE_SETTINGS = 0xC000000;
+
+    DolbyAtmos(int priority, int audioSession) {
+        super(EFFECT_TYPE_NULL, EFFECT_TYPE_DAP, priority, audioSession);
+    }
+
+    private static int int32ToByteArray(int value, byte[] dst, int index) {
+        dst[index++] = (byte) (value & 0xff);
+        dst[index++] = (byte) ((value >>> 8) & 0xff);
+        dst[index++] = (byte) ((value >>> 16) & 0xff);
+        dst[index] = (byte) ((value >>> 24) & 0xff);
+        return 4;
+    }
+
+    private static int byteArrayToInt32(byte[] ba) {
+        return ((ba[3] & 0xff) << 24) | ((ba[2] & 0xff) << 16)
+                | ((ba[1] & 0xff) << 8) | (ba[0] & 0xff);
+    }
+
+    private static int int32ArrayToByteArray(int[] src, byte[] dst, int index) {
+        for (int x : src) {
+            dst[index++] = (byte) ((x >>> 0) & 0xff);
+            dst[index++] = (byte) ((x >>> 8) & 0xff);
+            dst[index++] = (byte) ((x >>> 16) & 0xff);
+            dst[index++] = (byte) ((x >>> 24) & 0xff);
+        }
+        return src.length << 2;
+    }
+
+    private static int[] byteArrayToInt32Array(byte[] ba, int dstLength) {
+        int srcLength = ba.length >> 2;
+        if (dstLength > srcLength) {
+            dstLength = srcLength;
+        }
+        int[] dst = new int[dstLength];
+        for (int i = 0; i < dstLength; i++) {
+            dst[i] = ((ba[i * 4 + 3] & 0xff) << 24) | ((ba[i * 4 + 2] & 0xff) << 16)
+                    | ((ba[i * 4 + 1] & 0xff) << 8) | (ba[i * 4] & 0xff);
+        }
+        return dst;
+    }
+
+    private void setIntParam(int param, int value) {
+        byte[] buf = new byte[12];
+        int i = int32ToByteArray(param, buf, 0);
+        int32ToByteArray(value, buf, i + int32ToByteArray(1, buf, i));
+        checkStatus(setParameter(EFFECT_PARAM_CPDP_VALUES, buf));
+    }
+
+    private int getIntParam(int param) {
+        byte[] buf = new byte[12];
+        int32ToByteArray(param, buf, 0);
+        checkStatus(getParameter(EFFECT_PARAM_CPDP_VALUES + param, buf));
+        return byteArrayToInt32(buf);
+    }
+
+    void setDsOn(boolean on) {
+        setIntParam(EFFECT_PARAM_ENABLE, on ? 1 : 0);
+        super.setEnabled(on);
+    }
+
+    boolean getDsOn() {
+        return getIntParam(EFFECT_PARAM_ENABLE) == 1;
+    }
+
+    void setProfile(int index) {
+        setIntParam(EFFECT_PARAM_PROFILE, index);
+    }
+
+    int getProfile() {
+        return getIntParam(EFFECT_PARAM_PROFILE);
+    }
+
+    void resetProfileSpecificSettings() {
+        int profile = getProfile();
+        dlog("resetProfileSpecificSettings: profile=" + profile);
+        setIntParam(EFFECT_PARAM_RESET_PROFILE_SETTINGS, profile);
+    }
+
+    void setDapParameter(int profile, DsParam param, int values[]) {
+        dlog("setDapParameter: profile=" + profile + " param=" + param);
+        int length = values.length;
+        byte[] buf = new byte[(length + 4) * 4];
+        int i = int32ToByteArray(EFFECT_PARAM_SET_PROFILE_PARAMETER, buf, 0);
+        int i2 = i + int32ToByteArray(length + 1, buf, i);
+        int i3 = i2 + int32ToByteArray(profile, buf, i2);
+        int32ArrayToByteArray(values, buf, i3 + int32ToByteArray(param.id, buf, i3));
+        checkStatus(setParameter(EFFECT_PARAM_CPDP_VALUES, buf));
+    }
+
+    void setDapParameter(DsParam param, int values[]) {
+        setDapParameter(getProfile(), param, values);
+    }
+
+    void setDapParameterBool(DsParam param, boolean enable) {
+        setDapParameter(param, new int[]{enable ? 1 : 0});
+    }
+
+    void setDapParameterInt(DsParam param, int value) {
+        setDapParameter(param, new int[]{value});
+    }
+
+    int[] getDapParameter(int profile, DsParam param) {
+        dlog("getDapParameter: profile=" + profile + " param=" + param);
+        int length = param.length;
+        byte[] buf = new byte[(length + 2) * 4];
+        int i = (param.id << 16) + EFFECT_PARAM_GET_PROFILE_PARAMETER;
+        checkStatus(getParameter(i + (profile << 8), buf));
+        return byteArrayToInt32Array(buf, length);
+    }
+
+    int[] getDapParameter(DsParam param) {
+        return getDapParameter(getProfile(), param);
+    }
+
+    boolean getDapParameterBool(DsParam param) {
+        return getDapParameter(param)[0] == 1;
+    }
+
+    int getDapParameterInt(DsParam param) {
+        return getDapParameter(param)[0];
+    }
+
+    private static void dlog(String msg) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, msg);
+        }
+    }
+}

src/co/aospa/dolby/xiaomi/SummaryProvider.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *           (C) 2023-24 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby.xiaomi;
+
+import static com.android.settingslib.drawer.TileUtils.META_DATA_PREFERENCE_SUMMARY;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+
+import co.aospa.dolby.xiaomi.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+/** Provide preference summary for injected items. */
+public class SummaryProvider extends ContentProvider {
+
+    private static final String KEY_DOLBY = "dolby";
+
+    @Override
+    public Bundle call(String method, String uri, Bundle extras) {
+        final Bundle bundle = new Bundle();
+        String summary;
+        switch (method) {
+            case KEY_DOLBY:
+                summary = getDolbySummary();
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown method: " + method);
+        }
+        bundle.putString(META_DATA_PREFERENCE_SUMMARY, summary);
+        return bundle;
+    }
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+            String sortOrder) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        throw new UnsupportedOperationException();
+    }
+
+    private String getDolbySummary() {
+        final DolbyUtils dolbyUtils = DolbyUtils.getInstance(getContext());
+        final boolean dsOn = dolbyUtils.getDsOn();
+        if (!dsOn) {
+            return getContext().getString(R.string.dolby_off);
+        }
+        final String profileName = dolbyUtils.getProfileName();
+        if (profileName == null) {
+            return getContext().getString(R.string.dolby_on);
+        } else {
+            return getContext().getString(R.string.dolby_on_with_profile, profileName);
+        }
+    }
+
+}